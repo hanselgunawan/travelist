38e0aca31636e72c05c727997bb2706700dd20a2
hansel.tritama <hanselgunawan94@gmail.com>
2018-02-03 23:59:59 -0800

    Added header navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
 import React from "react";
+import SearchBar from "./SearchBar/SearchBar";
+import DropdownCity from "./DropdownCity/DropdownCity";
+import LoginNav from "./LoginNav/LoginNav";
 
 let textCenter = {
     textAlign:"center"
@@ -12,8 +15,12 @@ let marginTop = {
     marginTop:10
 };
 
+let navStyle = {
+    height:60
+};
+
 const Header = () =>
-    <nav className="navbar navbar-default navbar-inverse">
+    <nav className="navbar navbar-default navbar-inverse" style={navStyle}>
         <div className="container-fluid">
             <div className="row">
                 <div className="col-lg-2">
@@ -22,65 +29,12 @@ const Header = () =>
                     </div>
                 </div>
                 <div className="col-lg-7" style={marginTop}>
-                    <form>
-                        <div className="input-group">
-                            <input type="text" className="form-control" placeholder="Search"/>
-                                <div className="input-group-btn">
-                                    <button className="btn btn-default" type="submit">
-                                        <i class="glyphicon glyphicon-search"></i>
-                                    </button>
-                                </div>
-                        </div>
-                    </form>
+                    <SearchBar />
                 </div>
                 <div className="col-lg-3">
-                    <ul className="nav navbar-nav">
-                        <li className="dropdown">
-                            <a className="dropdown-toggle" data-toggle="dropdown"><b>City</b><span className="caret"></span></a>
-                            <ul className="dropdown-menu">
-                                <li><a>Los Angeles</a></li>
-                                <li><a>Chicago</a></li>
-                                <li><a>Seattle</a></li>
-                                <li><a>New York</a></li>
-                                <li><a>Whittier</a></li>
-                                <li><a>Glendale</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                    <DropdownCity />
                     <ul className="nav navbar-nav navbar-right">
-                        <li className="dropdown">
-                            <a className="dropdown-toggle" data-toggle="dropdown"><b><i className="fa fa-user"></i></b></a>
-                            <ul id="login-dp" className="dropdown-menu">
-                                <li>
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            <form className="form" method="post" action="login" id="login-nav">
-                                                <div className="form-group">
-                                                    <label className="sr-only" htmlFor="exampleInputEmail2">Email address</label>
-                                                    <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Email address" required />
-                                                </div>
-                                                <div className="form-group">
-                                                    <label className="sr-only" htmlFor="exampleInputPassword2">Password</label>
-                                                    <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Password" required />
-                                                        <div className="help-block text-right"><a href="">Forget the password ?</a></div>
-                                                </div>
-                                                <div className="form-group">
-                                                    <button type="submit" className="btn btn-primary btn-block">Sign in</button>
-                                                </div>
-                                                <div className="checkbox">
-                                                    <label>
-                                                        <input type="checkbox" />keep me logged-in
-                                                    </label>
-                                                </div>
-                                            </form>
-                                        </div>
-                                        <div className="bottom text-center">
-                                            New here ? <a><b>Join Us</b></a>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
-                        </li>
+                        <LoginNav />
                     </ul>
                 </div>
             </div>
